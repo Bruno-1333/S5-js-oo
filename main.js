ea8ed7a41ab9c79ab343829ebc3f012ecf07a780
Bruno-1333 <2295132rcegepoutaouais.qc.ca>
2023-02-08 15:47:25 -0500

    version 08/02/2023

--- main.js
+++ main.js
@@ -1,43 +1,60 @@
-// Classe Fruit qui représente chaque fruit individuel dans le panier
-class Fruit {
-    // Le constructeur prend en entrée le nom du fruit et son prix
-    constructor(nom, prix, quantite, total) {
+class Fruit{
+    constructor(nom, prix, quantite, sousTotal) {
         this.nom = nom;
         this.prix = prix;
         this.quantite = quantite;
-        this.total = total;
+        this.sousTotal = sousTotal;
     }
-    // Ajouter les fruit au panier
+
+    toString(){
+        return this.quantite + " " + this.nom + " à " + this.prix + " : " + this.sousTotal;
+    }
 }
 
-function Ajouter(fruit) {
-    let peches = new Fruit("peches", 10, 0,0 );
-     peches.quantite = document.getElementById("nbPeches");
-     peches.total = document.getElementById("totalPeches");
-
-    let poires = new Fruit("poires", 12, 0,0 );
-    poires.quantite = document.getElementById("nbPoires");
-    poires.total = document.getElementById("totalPoires");
-
-    let pommes = new Fruit("pommes", 11, 0,0 );
-    pommes.quantite = document.getElementById("nbPommes");
-    pommes.total = document.getElementById("totalPommes");
-
-
-    switch (fruit) {
-        case "peches" :
-            peches.total.innerHTML = peches.prix * parseInt(peches.quantite.value);
-            break;
-        case "poires" :
-            poires.total.innerHTML = poires.prix * parseInt(poires.quantite.value);
-            break;
-        case "pommes" :
-            pommes.total.innerHTML = pommes.prix * parseInt(pommes.quantite.value);
-            break;
-
+class Panier{
+    constructor(peches, poires, pommes, total) {
+        this.peches = peches;
+        this.poires = poires;
+        this.pommes = pommes
+        this.total = total;
     }
 
+    toString(){
+        return this.peches + "\n" + this.poires + "\n" + this.pommes, + "\n" + this.total;
+    }
+}
 
+function valider(fruit){
+    // Récupérer les variables
+    let nbFruit = document.getElementById("nb"+fruit).value;
+    let prix = document.getElementById("prix"+fruit).innerText; // cherche dans un table
+    let sousTotal = 0;
+
+    // Valider le nombre de fruits
+    if (nbFruit < 0 || nbFruit > 24)
+    {
+        document.getElementById("erreur"+fruit).innerText = "Doit être entre 0 et 24";
+    }
+    // Afficher le sous-total
+    else {
+        document.getElementById("erreur"+fruit).innerText = "";
+        sousTotal = +nbFruit * +prix;
+        document.getElementById("sousTotal"+fruit).innerText = sousTotal;
+    }
+
+    // Créer les objets fruits
+    nouveauFruit = new Fruit(fruit, prix, nbFruit, sousTotal);
+}
+
+function envoyer(){
+
+    //inscririre les données dans la session
+    sessionStorage.setItem("nbPommes", document.getElementById("nbPommes").value);
+
+    // Créer le panier
+    let monPanier = new Panier(nouveauFruit, 0, 0, nouveauFruit.sousTotal);
+    sessionStorage.setItem("panier", monPanier);
+    return true;
 }
 
 
